9109a76190d6d826200f2f862b547fbde2605123
Wentana Ghile <wentanaghile@MacBook-Pro-3.local>
2023-09-18 13:50:37 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
+//Setting Up Server
 const express = require('express')
 const pokemon = require('./models/pokemon')
 const app = express()
 const port = 3000
 
+//Setting Default Engine and Extension
+const jsxEngine = require('jsx-view-engine')
+app.set('view engine', 'jsx');
+app.engine('jsx', jsxEngine());
+
+//Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon)
+    res.render('Index')
 })
 
 app.listen(port, () => {
---
+++ views/Index.jsx
@@ -0,0 +1,13 @@
+const React = require('react')
+
+class Index extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>See All The Pokemon!</h1>
+            </div>
+        )
+    }
+}
+
+module.exports = Index
