c4c8cb5fb29106d8a7a7167fc4a146427d4e3843
Wentana Ghile <wentanaghile@Wentana-MacBook-Pro.local>
2023-10-04 23:56:42 -0400

    added images to public folder and fixed layout issues

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 //Routes
     app.get('/', (req, res) => {
